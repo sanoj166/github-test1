3d4b34149f85f16d624558452d8cff677dd8fabd
sanoj166 <j_haggu@hotmail.com>
2023-09-06 14:19:30 +0200

    Add project files.

---
+++ github test1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace github_test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hallå eller!");
+        }
+    }
+}
